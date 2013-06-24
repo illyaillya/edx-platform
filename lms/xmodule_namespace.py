3eba65c003ee2e991ab0a9e2cd2391c3ad48f3ea
Don Mitchell <dmitchell@edx.org>
2013-06-24 14:10:52 -0400

    Move defaults from yaml templates to field definitions.
    This standardizes the XModule field default values to be the same as the
    values that are presented by studio when a component is added to a
    course.
    
    TODO: Make from_xml in descriptors that had defaults changed give the
    old defaults for xml courses

--- lms/xmodule_namespace.py
+++ lms/xmodule_namespace.py
@@ -3,6 +3,8 @@ Namespace that defines fields common to all blocks used in the LMS
 """
 from xblock.core import Namespace, Boolean, Scope, String, Float
 from xmodule.fields import Date, Timedelta
+from xmodule.course_module import CourseDescriptor
+from xmodule.capa_module import CapaDescriptor
 
 
 class LmsNamespace(Namespace):
@@ -25,18 +27,15 @@ class LmsNamespace(Namespace):
         scope=Scope.settings,
     )
 
-    start = Date(help="Start time when this module is visible", scope=Scope.settings)
-    due = Date(help="Date that this problem is due by", scope=Scope.settings)
+    start = CourseDescriptor.start
+    due = CapaDescriptor.due
     source_file = String(help="source file name (eg for latex)", scope=Scope.settings)
     giturl = String(help="url root for course data git repository", scope=Scope.settings)
     xqa_key = String(help="DO NOT USE", scope=Scope.settings)
     ispublic = Boolean(help="Whether this course is open to the public, or only to admins", scope=Scope.settings)
-    graceperiod = Timedelta(
-        help="Amount of time after the due date that submissions will be accepted",
-        scope=Scope.settings
-    )
-    showanswer = String(help="When to show the problem answer to the student", scope=Scope.settings, default="closed")
-    rerandomize = String(help="When to rerandomize the problem", default="always", scope=Scope.settings)
+    graceperiod = CapaDescriptor.graceperiod
+    showanswer = CapaDescriptor.showanswer
+    rerandomize = CapaDescriptor.rerandomize
     days_early_for_beta = Float(
         help="Number of days early to show content to beta users",
         default=None,
